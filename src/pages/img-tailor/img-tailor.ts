6df6b7c5f862f8fe37ce7ec270798d15b04099b5
qin <990939875@qq.com>
2018-04-20 18:09:15 +0800

    canvas

--- src/pages/img-tailor/img-tailor.ts
+++ src/pages/img-tailor/img-tailor.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Content } from 'ionic-angular';
 import { Gesture } from 'ionic-angular';
 /**
  * Generated class for the ImgTailorPage page.
@@ -14,33 +14,120 @@ import { Gesture } from 'ionic-angular';
   templateUrl: 'img-tailor.html',
 })
 export class ImgTailorPage {
-  @ViewChild('image') element: any;
+  @ViewChild('tailorContainer') tailorContainer: ElementRef;
   @ViewChild('canvas') canvas: ElementRef;
-  private _CANVAS: any;
-  private _CONTEXT: any;
+  @ViewChild('mask') mask: ElementRef;
+  @ViewChild(Content) content: Content;
+  private _CANVAS;                      // 图片层
+  private _MASK;                        // 遮罩层
+  private _CONTEXT;                     // 图片绘制区域
+  private COVER_BOX_CONTENT;            // 遮罩绘制区域
+  private width: number;            // 遮罩绘制区域
+  private height: number;            // 遮罩绘制区域
+  private _imgs = new Image();          // 待裁剪的图片
+  private stepClear = 1;                // 别忘记这一步 
   private gesture: Gesture;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
   ) {
-
+    this._imgs.src = "assets/imgs/underwater1.jpg";
   }
 
   ionViewDidLoad() {
-    this._CANVAS = this.canvas.nativeElement;
-    this._CANVAS.width = 412;
-    this._CANVAS.height = 700;
-    this.initialiseCanvas();
-    this.drawCircle();
-    this.gesture = new Gesture(this.element.nativeElement);
-    console.log('ionViewDidLoad ImgTailorPage');
+    this.gesture = new Gesture(this.tailorContainer.nativeElement);
     this.gesture.listen();
 
-    //监听缩放
+    this.width = this.tailorContainer.nativeElement.offsetWidth;
+    this.height = this.tailorContainer.nativeElement.offsetHeight;
     this.gesture.on('pinch', e => this.pinch(e));
+    this.initCanvas(this.width, this.height)
+    this.clearArc(this.width / 2, this.width / 2 + 50, (this.width / 2) - 30);
+    this.drawImage(this._imgs)
   }
+
+  /**
+   * 初始化 canvas
+   * 
+   * @author qin
+   * @param {number} width 
+   * @param {number} height 
+   * @memberof ImgTailorPage
+   */
+  initCanvas(width: number, height: number): void {
+    this._CANVAS = this.canvas.nativeElement;
+    this._MASK = this.mask.nativeElement;
+    this._CONTEXT = this._CANVAS.getContext('2d');
+    this.COVER_BOX_CONTENT = this._MASK.getContext('2d');
+    this._CANVAS.width = width;
+    this._CANVAS.height = height;
+    this._MASK.width = width;
+    this._MASK.height = height;
+    this.COVER_BOX_CONTENT.fillStyle = "rgba(0,0,0,.5)";
+    this.COVER_BOX_CONTENT.fillRect(0, 0, width, height);
+  }
+
+  /**
+   * 绘制图片
+   * 
+   * @author qin
+   * @param {*} img 
+   * @memberof ImgTailorPage
+   */
+  drawImage(img: any): void {
+    this._imgs.onload = () => {
+      this._CONTEXT.drawImage(img, 0, 0);
+      setTimeout(() => {
+        this.canvaScale(0.5, this._imgs)
+      }, 5000);
+    }
+  }
+
+
+  /**
+   * 再遮罩层掏出选择区域
+   * 
+   * @author qin
+   * @param {any} x 
+   * @param {any} y 
+   * @param {any} radius 
+   * @memberof ImgTailorPage
+   */
+  clearArc(x, y, radius): void {//圆心(x,y)，半径radius     
+    var calcWidth = radius - this.stepClear;
+    var calcHeight = Math.sqrt(radius * radius - calcWidth * calcWidth);
+    var posX = x - calcWidth;
+    var posY = y - calcHeight;
+
+    var widthX = 2 * calcWidth;
+    var heightY = 2 * calcHeight;
+
+    if (this.stepClear <= radius) {
+      this.COVER_BOX_CONTENT.clearRect(posX, posY, widthX, heightY);
+      this.stepClear += 1;
+      this.clearArc(x, y, radius);
+    }
+  }
+
+
+  /**
+   * 图片缩放
+   * 
+   * @author qin
+   * @param {number} scale 
+   * @param {*} img 
+   * @memberof ImgTailorPage
+   */
+  canvaScale(scale: number, img: any): void {
+    let rectCenterPoint = { x: this.width / 2, y: this.height / 2 };// 矩形中心点
+    this._CONTEXT.clearRect(0, 0, this.width, this.height)
+    this._CONTEXT.translate(rectCenterPoint.x * (1 - scale), rectCenterPoint.y * (1 - scale));
+    this._CONTEXT.scale(scale, scale);
+    this._CONTEXT.drawImage(img, 0, 0);
+  }
+
   pinch(e): void {
-    console.log(e.scale)
+    this.canvaScale(e.scale, this._imgs)
   }
   pan(e): void {
     console.log("pan" + e)
@@ -48,31 +135,5 @@ export class ImgTailorPage {
 
   back(): void {
     this.navCtrl.pop();
-  }
-
-  initialiseCanvas() {
-    if (this._CANVAS.getContext) {
-      this.setupCanvas();
-    }
-  }
-  setupCanvas() {
-    this._CONTEXT = this._CANVAS.getContext('2d');
-    this._CONTEXT.fillStyle = "rgba(0,0,0,0.5)";
-    this._CONTEXT.fillRect(0, 0, this._CANVAS.width, this._CANVAS.height);
-  }
-  clearCanvas() {
-    this._CONTEXT.clearRect(0, 0, this._CANVAS.width, this._CANVAS.width);
-    this.setupCanvas();
-  }
-
-  drawCircle() {
-    this.clearCanvas();
-    this._CONTEXT.beginPath();
-
-    // x, y, radius, startAngle, endAngle
-    this._CONTEXT.arc(this._CANVAS.width / 2, this._CANVAS.width / 1.8,  this._CANVAS.width/2.2, 0, 2 * Math.PI);
-    this._CONTEXT.lineWidth = 1;
-    this._CONTEXT.strokeStyle = '#ffffff';
-    this._CONTEXT.stroke();
   }
 }
